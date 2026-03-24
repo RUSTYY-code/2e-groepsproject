1fc2ee7f39fb1dcd4337c6f0d705c181c5196e4f
Jamie-rocvf <2227916@talnet.nl>
2026-03-24 09:50:24 +0100

    Update cart-icon-counter.js

--- cart-icon-counter.js
+++ cart-icon-counter.js
@@ -1,9 +1,26 @@
 const plantAdd = document.querySelector('.cartButton');
-        const cartCount = document.querySelector('.icon-cart span');
-        plantAdd.addEventListener('click', function () {
-            let current = parseInt(cartCount.innerText);
-            cartCount.innerText = current + 1;
-            if (current >= 9) {
-                cartCount.innerText = '9+';
-            }
-        });
+const cartCount = document.querySelector('.icon-cart span');
+const plantRem = document.querySelector('.cartButton2');
+
+const savedCount = localStorage.getItem('cartCount');
+if (savedCount !== null) {
+    cartCount.innerText = parseInt(savedCount) >= 10 ? '9+' : savedCount;
+}
+
+function updateDisplay(num) {
+    cartCount.innerText = num >= 10 ? '9+' : num;
+    localStorage.setItem('cartCount', num);
+}
+
+plantAdd.addEventListener('click', function () {
+    let num = parseInt(localStorage.getItem('cartCount')) || 0;
+    num += 1;
+    updateDisplay(num);
+});
+
+plantRem.addEventListener('click', function () {
+    let num = parseInt(localStorage.getItem('cartCount')) || 0;
+    if (num <= 0) return;
+    num -= 1;
+    updateDisplay(num);
+});
