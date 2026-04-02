e18494035560746684109186c9925509de5147d7
Brooklyn2o31342131 <Brooklynhoefdraad@hotmail.com>
2026-04-02 13:02:33 +0200

    Update shoppingcart.js

--- shoppingcart.js
+++ shoppingcart.js
@@ -9,11 +9,9 @@ let count = 0;
 addButtons.forEach((button) => {
     button.addEventListener("click", () => {
 
-        // Zoek het select-element in dezelfde product-row
         const select = button.closest(".product-row").querySelector("select");
         const amount = parseInt(select.value);
 
-        // Tel het aantal erbij
         count += amount;
 
         cartCount.textContent = count;
