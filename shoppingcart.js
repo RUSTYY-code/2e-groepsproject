46decb6bee182bc5acbf08de8d6e5023dcdf06de
Brooklyn2o31342131 <Brooklynhoefdraad@hotmail.com>
2026-04-09 13:21:52 +0200

    Uppdate Shopping cart

--- shoppingcart.js
+++ shoppingcart.js
@@ -4,15 +4,26 @@ const addButtons = document.querySelectorAll(".add-to-cart-btn");
 let count = parseInt(localStorage.getItem("cartCount"), 10) || 0;
 
 cartCount.textContent = count;
+
 addButtons.forEach((button) => {
     button.addEventListener("click", () => {
         const select = button.closest(".product-row").querySelector("select");
         const amount = parseInt(select.value, 10);
 
         count += amount;
- 
-        cartCount.textContent = count;
 
+        cartCount.textContent = count;
         localStorage.setItem("cartCount", count);
     });
+});
+
+const removeButton = document.querySelector(".remove-from-cart-btn");
+
+removeButton.addEventListener("click", () => {
+    if (count > 0) {
+        count -= 1;
+    }
+
+    cartCount.textContent = count;
+    localStorage.setItem("cartCount", count);
 });
