09a84d5fc5989eb8e52e72dae833a0a0ad9e038e
Brooklyn2o31342131 <Brooklynhoefdraad@hotmail.com>
2026-04-09 12:35:02 +0200

    hello

--- shoppingcart.js
+++ shoppingcart.js
@@ -6,7 +6,7 @@ const addButtons = document.querySelectorAll(".add-to-cart-btn");
 let count = 0;
 
 addButtons.forEach((button) => {
-    button.addEventListener("click", () => {
+    button.addEventListener("click", () => {    
 
         const select = button.closest(".product-row").querySelector("select");
         const amount = parseInt(select.value);
